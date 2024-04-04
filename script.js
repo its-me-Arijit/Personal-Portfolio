93dffef6314ce5ba5cafd039e0710806cfe8548e
Arijit Ghosh <arijit.haldia2002@gmail.com>
2024-04-04 14:14:26 +0530

    added URL BANK chrome extention  and removed random quote generator with the necessary files for each

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+function downloadZipFile(fileName) {
+  const link = document.createElement('a');
+  link.href = `./assets/${fileName}`;
+  link.download = fileName;
+  link.click();
+}
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+// Add this function to handle the download of the "URL BANK.zip" file
+function downloadURLBankZipFile() {
+  downloadZipFile('URL_BANK.zip');
 }
